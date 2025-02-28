135978ccc34f73397dff633ee4985b3d0108482a
gladw-in <89470287+gladw-in@users.noreply.github.com>
2025-02-28 18:09:58 +0530

    Fix bug in CodeEditor component

--- Frontend/src/CodeEditor.jsx
+++ Frontend/src/CodeEditor.jsx
@@ -718,7 +718,11 @@ const CodeEditor = ({
         </div>
 
         <pre className="select-text font-mono text-xs font-semibold lg:text-sm  min-h-20 max-h-[295px] overflow-auto p-3 rounded-b-lg [scrollbar-width:thin] bg-[#eaeaea] text-[#292929] dark:bg-[#262636] dark:text-[#24a944]">
-          {output.replace(/```/g, "") || "Run your code to see output here..."}
+          {output
+            .replace(/^```text[\r\n]*/m, "")
+            .replace(/^```[\r\n]*/m, "")
+            .replace(/[\r\n]*```$/m, "") ||
+            "Run your code to see output here..."}
         </pre>
       </div>
       <p className="ml-2 text-sm text-gray-500 italic">
